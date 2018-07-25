0ff026ae74f3098b9a7065d209fdcda88290721a
SilviuIsidore <silviu@isidor.com>
2018-07-25 09:55:42 -0700

    Initial Commit

---
+++ layerResizeProblem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layerResizeProblem
+//
+//  Created by Silviu on 25/07/2018.
+//  Copyright © 2018 Silviu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
